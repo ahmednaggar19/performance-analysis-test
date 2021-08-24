c3dc855d1c6b051e90866f47c344fe9fb00c3cbc
ahmednaggar19 <ahmed.naggar@incorta.com>
2021-08-24 23:36:20 +0200

    Fixed Dependency problem

--- src/main/java/PerformanceTest.java
+++ src/main/java/PerformanceTest.java
@@ -1,9 +1,11 @@
+import dark.DarkMagician;
+
 public class PerformanceTest {
-    static public void main(String[] agrs) {
-        for (int i = 0; i < 10; i++) {
+    public static void main(String[] agrs) {
+        for (int i = 0; i < 5; i++) {
             DarkMagician.curseTheCode();
             DarkMagician.doTheSpill();
-            DarkMagician.swayYouWound();
+            DarkMagician.swayYourWound();
         }
     }
 }
