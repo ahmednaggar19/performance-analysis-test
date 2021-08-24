2589155d49f77b7661ccc6a7c365718fbce91b34
ahmednaggar19 <ahmed.naggar@incorta.com>
2021-08-24 23:15:02 +0200

    First commit

---
+++ src/main/java/PerformanceTest.java
@@ -0,0 +1,9 @@
+public class PerformanceTest {
+    static public void main(String[] agrs) {
+        for (int i = 0; i < 10; i++) {
+            DarkMagician.curseTheCode();
+            DarkMagician.doTheSpill();
+            DarkMagician.swayYouWound();
+        }
+    }
+}
